1dbf5d0b56210fd2b3678c13cd7da3b3141d4c89
Leonardo Rojas <leonardo.rojas@fairplaylabs.com>
2015-07-13 22:00:22 -0600

    Implementing Unity base project.

--- UnityProject/New Unity Project/Assets/Game/Scripts/Input/CharacterInput.cs
+++ UnityProject/New Unity Project/Assets/Game/Scripts/Input/CharacterInput.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public abstract class  CharacterInput 
+public abstract class CharacterInput 
 {
 	#region Variables
 	
--- UnityProject/New Unity Project/Assets/Game/Scripts/Input/PS3CharacterInput.cs
+++ UnityProject/New Unity Project/Assets/Game/Scripts/Input/PS3CharacterInput.cs
@@ -3,39 +3,28 @@ using System.Collections;
 
 public class PS3CharacterInput : CharacterInput 
 {
-	#region Variables
-
-	private float _lastMovementRotation = 0;
-
-	#endregion
-
 	#region Methods
 
 	public override void UpdateInput()
 	{
-		_isJumpingButtonDown = Input.GetKeyDown(KeyCode.Space);
-		
-		//UpdateRotation();
-		//UpdateMovement();
-	}
-	
-	/*public override Vector3 GetMovement()
-	{
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical   = Input.GetAxis("Vertical");
-		
-		return new Vector3(moveHorizontal, 0.0f, moveVertical);
+		_isJumpingButtonDown = Input.GetKeyDown(KeyCode.JoystickButton14);
+		_rotation 			+= Input.GetAxis("Horizontal") * Constants.KEYBOARD_ROTATION_SPEED;
+
+		UpdateMovement();
 	}
 
-	public override bool IsJumpButtonDown()
+	private void UpdateMovement()
 	{
-		return Input.GetKeyDown(KeyCode.JoystickButton14);
+		float verticalAxis = Input.GetAxis("Vertical");
+
+		if(verticalAxis != 0)
+		{
+			Vector2 movement2D = MathUtils.GetPointAtDistance(Vector2.zero, verticalAxis, _rotation);
+			_movement 		   = new Vector3(movement2D.y, 0, movement2D.x);
+		}
+		else
+			_movement = Vector3.zero;
 	}
-
-	public override void SetMovementRotation(float rotation)
-	{
-
-	}*/
 	
 	#endregion
 }
