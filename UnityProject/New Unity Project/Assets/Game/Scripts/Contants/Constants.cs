58c8f0cd9deef70bc8b8fa3822da47d00c6de828
Leonardo Rojas <leonardo.rojas@fairplaylabs.com>
2015-07-14 21:59:49 -0600

    Implementing Unity base project.

--- UnityProject/New Unity Project/Assets/Game/Scripts/Contants/Constants.cs
+++ UnityProject/New Unity Project/Assets/Game/Scripts/Contants/Constants.cs
@@ -11,7 +11,9 @@ public class Constants
 	public const float CHARACTER_DEFAULT_SPEED			   = 10.0f;
 	public const float CHARACTER_MOVEMENT_LERP_SPEED	   = 0.1f;
 	public const float CHARACTER_JUMP_FORCE 			   = 10;
+
 	public const float KEYBOARD_ROTATION_SPEED			   = 2f;
-	
+	public const float MOUSE_ROTATION_SPEED				   = 8f;
+
 	#endregion
 }
---
+++ UnityProject/New Unity Project/Assets/Game/Scripts/Input/KeyboardAndMouseCharacterInput.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardAndMouseCharacterInput : CharacterInput 
+{
+	#region Methods
+
+	public override void UpdateInput()
+	{
+		_isJumpingButtonDown = Input.GetKeyDown(KeyCode.Space);
+
+		UpdateRotation();
+		UpdateMovement();
+	}
+
+	private void UpdateRotation()
+	{
+		_rotation += Input.GetAxis("Mouse X") * Constants.MOUSE_ROTATION_SPEED;
+	}
+	
+	private void UpdateMovement()
+	{
+		if(Input.GetKey(KeyCode.W))
+		{
+			Vector2 movement2D = MathUtils.GetPointAtDistance(Vector2.zero, 1, _rotation);
+			_movement 		   = new Vector3(movement2D.y, 0, movement2D.x);
+		}
+		else if(Input.GetKey(KeyCode.S))
+		{
+			Vector2 movement2D = MathUtils.GetPointAtDistance(Vector2.zero, 1, _rotation - 180);
+			_movement 		   = new Vector3(movement2D.y, 0, movement2D.x);
+		}
+		else
+			_movement = Vector3.zero;
+	}
+
+	#endregion
+}
