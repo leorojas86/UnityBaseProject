89252379ac5ddc44e97e728f7e1b7b8178e02166
Leonardo Rojas <leonardo.rojas@fairplaylabs.com>
2015-07-14 21:19:06 -0600

    Implementing Unity base project.

---
+++ UnityProject/New Unity Project/Assets/Game/Scripts/Scenes/TestSceneController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSceneController : MonoBehaviour 
+{
+	#region Methods
+	
+	void Start() 
+	{
+		Application.targetFrameRate = 60;
+		Time.fixedDeltaTime 		= 1f / 60f;
+	}
+
+	#endregion
+}
