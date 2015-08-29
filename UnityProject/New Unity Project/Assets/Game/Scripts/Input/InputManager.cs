142b623ce1bbfd6da074a8d501b6f5ac9e383a3b
Leonardo Rojas <leonardo.rojas@fairplaylabs.com>
2015-08-29 11:50:38 -0600

    Implementing Unity base project.

---
+++ UnityProject/New Unity Project/Assets/Game/Scripts/Input/InputManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager 
+{
+	#region Variables
+
+	private static InputManager _instance = null;
+
+	#endregion
+
+	#region Properties
+
+	public static InputManager Instance
+	{
+		get 
+		{ 
+			if(_instance != null)
+				return _instance;
+
+
+			_instance = new InputManager();
+			return _instance;
+		}
+	}
+
+	#endregion
+
+	#region Constructors
+
+	private InputManager()
+	{
+	}
+
+	#endregion
+
+	#region Methods
+
+	public CharacterInput DetectNewCharacterInput()
+	{
+		return null;
+	}
+
+	#endregion
+}
---
+++ UnityProject/New Unity Project/Assets/Game/Scripts/Profiling/FPSCounter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FPSCounter : MonoBehaviour 
+{
+	#region Variables
+
+	private Text _text    	= null;
+	private int _frames     = 0;
+	private int _lastSecond = 0;
+
+	#endregion
+
+	#region Methods
+
+	void Awake()
+	{
+		_text = GetComponent<Text>();
+	}
+
+	void Update() 
+	{
+		_frames++;
+
+		if(Time.time > _lastSecond + 1)
+		{
+			_text.text  = "FPS: " + _frames;
+			_frames     = 0;
+			_lastSecond = (int)Time.time;
+		}
+	}
+
+	#endregion
+}
